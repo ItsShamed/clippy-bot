774cad441556d3a465d4ef1be8ddd0b9f445a6e6
Ryuki <thedjplays.gaming@gmail.com>
2022-02-11 02:48:06 +0100

    Base bot

--- index.js
+++ index.js
@@ -1,23 +1,78 @@
-const { Client, Intents } = require("discord.js");
+const { Client, Intents, MessageEmbed, Message } = require("discord.js");
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 });
+
+const contentTypes = require('./contentTypes.json');
+
+String.prototype.format = function() {
+  var args = arguments;
+  return this.replace(/{([0-9]+)}/g, function(match, index) {
+    return typeof args[index] == 'undefined' ? match : args[index];
+  });
+};
+
 
 client.once('ready', () => {
   console.log('Bot is connected.');
 });
 
 client.on('messageCreate', (message) => {
+
+  if (message.author.id === client.user.id)
+    return;
+
   console.log(`Message received from ${message.author.username}.`);
+
   if (message.attachments.size <= 0) {
     console.log("It has no attachments.")
   }
   else {
+
     console.log(`It has ${message.attachments.size} attachments which are:`)
-    message.attachments.forEach(attachments => {
-      console.log(` - ${attachments.name} (${attachments.contentType})`)
+
+    let validAttachments = message.attachments.filter(
+      a => contentTypes[a.contentType] != undefined);
+    let reply = {
+      content: `Detected ${validAttachments.size} attachments`,
+      embeds: []
+    }
+
+    validAttachments.forEach(attachment => {
+
+      let embed = new MessageEmbed();
+
+      console.log(` - ${attachment.name} (${attachment.contentType})`);
+
+      if (contentTypes[attachment.contentType] != undefined) {
+
+        let contentType = contentTypes[attachment.contentType];
+
+        embed.setFooter({
+          text: contentType.type
+        }).setTitle(attachment.name)
+          .setDescription('Click the links below to open the file in your ' +
+            'browser');
+
+        contentType.viewers.forEach(viewer => {
+          embed.addField(
+            viewer.name,
+            `[Click here](${viewer.baseUrl.format(attachment.url)})`,
+            true
+          );
+
+        });
+      }
+      else {
+
+      }
+      reply.embeds.push(embed);
     });
+
+    if (validAttachments.size > 0) {
+      message.reply(reply);
+    }
   }
 });
 
