d3c9da0ab64fda6cfe5e6ba19bd84c12d40fcdc2
Ryuki <thedjplays.gaming@gmail.com>
2022-02-12 01:33:56 +0100

    Fix page wait

--- screenshot.js
+++ screenshot.js
@@ -1,5 +1,36 @@
 const puppeteer = require('puppeteer');
 
+const waitTillHTMLRendered = async (page, timeout = 30000) => {
+  const checkDurationMsecs = 1000;
+  const maxChecks = timeout / checkDurationMsecs;
+  let lastHTMLSize = 0;
+  let checkCounts = 1;
+  let countStableSizeIterations = 0;
+  const minStableSizeIterations = 3;
+
+  while (checkCounts++ <= maxChecks) {
+    let html = await page.content();
+    let currentHTMLSize = html.length;
+
+    let bodyHTMLSize = await page.evaluate(() => document.body.innerHTML.length);
+
+    console.log('last: ', lastHTMLSize, ' <> curr: ', currentHTMLSize, " body html size: ", bodyHTMLSize);
+
+    if (lastHTMLSize != 0 && currentHTMLSize == lastHTMLSize)
+      countStableSizeIterations++;
+    else
+      countStableSizeIterations = 0; //reset the counter
+
+    if (countStableSizeIterations >= minStableSizeIterations) {
+      console.log("Page rendered fully..");
+      break;
+    }
+
+    lastHTMLSize = currentHTMLSize;
+    await page.waitForTimeout(checkDurationMsecs);
+  }
+};
+
 let getScreenshot = async (browser, url, attempts) => {
 
   async function r(url, attempts) {
@@ -15,7 +46,7 @@ let getScreenshot = async (browser, url, attempts) => {
       console.log(`Going to ${url}`)
       await page.goto(url);
       console.log("Waiting for page to load...")
-      await page.waitForNetworkIdle();
+      await waitTillHTMLRendered(page);
       console.log("Taking screenshot...")
       const data = await page.screenshot();
       await page.close();
